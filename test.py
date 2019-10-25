71625dd1c31203c58ba1f726679414f1c86f35fa
candidate <candidate@programmr.com>
2019-10-25 08:40:04 +0000

    FINISH TEST

--- test.py
+++ test.py
@@ -57,16 +57,11 @@ with detection_graph.as_default():
     with tf.Session(graph=detection_graph) as sess:
         for image_path in TEST_IMAGE_PATHS:
             image = Image.open('TESTIMAGE.jpg')
-            # the array based representation of the image will be used later in order to prepare the
-            # result image with boxes and labels on it.
             image_np = load_image_into_numpy_array(image)
-            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+
             image_np_expanded = np.expand_dims(image_np, axis=0)
             image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-            # Each box represents a part of the image where a particular object was detected.
             boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-            # Each score represent how level of confidence for each of the objects.
-            # Score is shown on the result image, together with the class label.
             scores = detection_graph.get_tensor_by_name('detection_scores:0')
             classes = detection_graph.get_tensor_by_name('detection_classes:0')
             num_detections = detection_graph.get_tensor_by_name('num_detections:0')
